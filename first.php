f1e309524feca93f452fadfaa32f790f63c58472
Atri1731 <atripatel11004@gmail.com>
2025-06-29 12:37:24 +0530

    2nd commit

---
+++ first.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    echo"Enter num ";
+    $a=10;
+    $b=20;
+    if($a>$b)
+    {
+        echo"a is big"
+    }else{
+        echo"b is max"
+    }
+    ?>
+</body>
+</html>
